46452ed88ef4a5d078d399a4c1835804e8fb28b4
Bogdan1000pink <124085530+Bogdan1000pink@users.noreply.github.com>
2025-08-10 10:45:07 -0700

    Add files via upload
    Forgot to update these for the 3.0 update, also removed some useless junk :p

---
+++ item_mapping.lua
@@ -0,0 +1,23 @@
+-- use this file to map the AP item ids to your items
+-- first value is the code of the target item and the second is the item type (currently only "toggle", "progressive" and "consumable" but feel free to expand for your needs!)
+-- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
+ITEM_MAPPING = {
+    [27] = {"udjat", "toggle"},
+    [28] = {"hedjet", "toggle"},
+    [29] = {"crown", "toggle"},
+    [30] = {"ankh", "toggle"},
+    [31] = {"TabletOfDestiny", "toggle"},
+    [32] = {"excalibur", "toggle"},
+    [33] = {"scepter", "toggle"},
+    [34] = {"HouYisBow", "toggle"},
+    [35] = {"ArrowOfLight", "toggle"},
+    [36] = {"ushabti", "toggle"},
+    [37] = {"hp", "consumable"},
+    [38] = {"bombs", "consumable"},
+    [39] = {"ropes", "consumable"},
+    [40] = {"paste", "toggle"},
+    [42] = {"compass", "progressive"},
+    [43] = {"eggplant", "toggle"},
+    [44] = {"COCheckpoint", "consumable"},
+    [49] = {"secondworld", "progressive"}
+}
